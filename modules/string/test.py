53a5710768da9939718a3625477dc8cf6b0f8e03
Vladimir Baryshev <vbaryashev@gmail.com>
2017-06-28 21:28:55 +0300

    Added parentes and count

--- modules/string/test.py
+++ modules/string/test.py
@@ -3,5 +3,18 @@ from modules.tester import *
 
 equality_case(trim('  a'), 'a', 'Should trim spaces from start of string')
 equality_case(trim('a   '), 'a', 'Should trim spaces from end of string')
+
 equality_case(kebabToSnake("kabeab-to-snake"), 'kabeab_to_snake', 'Should change all - to _')
 equality_case(kebabToCamel("kabeab-to-camel"), 'kabeabToCamel', 'Should return camelCase string')
+
+equality_case(parentes("1+(2+3)"), (2, "2+3"), "Should work correctly")
+equality_case(parentes("1+3"), None, "Should return None if not found")
+
+equality_case(count("(((", "("), (3), "Should work correctly") 
+equality_case(count("+-+++--", "+"), (4), "Should work correctly") 
+equality_case(count("aAaAAA", "a"), (2), "Should work correctly") 
+equality_case(count("?###!%", "?"), (1), "Should work correctly") 
+equality_case(count("?###!%", "#"), (3), "Should work correctly") 
+
+
+
