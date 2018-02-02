45bc79d82a970cb63eecfd4a1112dcf1c22bdf43
Vladimir Baryshev <vbaryashev@gmail.com>
2018-02-02 23:19:43 +0300

    hw19 update

--- app/pkgs/tester/TestCase.py
+++ app/pkgs/tester/TestCase.py
@@ -12,12 +12,17 @@ checks = {
     'smaller': lambda a, b: a < b,
     'raise': raise_check,
     'has_attr': lambda a, b: hasattr(a, b),
-}
+    'type': lambda a, b: type(a) == b,
+    'instance': lambda a, b: instance(a, b),
+    'has_attr': lambda a, b: hasattr(a, b),
+    'has_method': lambda a, b: hasattr(a, b) and callable(getattr(a, b))
+    }
 
 class TestCase:
     def __init__(self, message):
         self.message = message
-        return 
+        self.res_inversion = None
+        return
 
     def _check_has_attr(self):
         if not hasattr(self, 'given'):
@@ -32,11 +37,16 @@ class TestCase:
             'condition': condition
         }
 
+    def _not(self):
+        self.res_inversion = True
+        return self
+
     def _exec_test(self, type_of_test, expected):
         self._check_has_attr()
         status = checks[type_of_test](self.given, expected)
+        if self.res_inversion == True:
+            status = not status
         return self._result_struct(expected, status, type_of_test)
-
 
     def expect(self, given):
         self.given = given
@@ -54,26 +64,17 @@ class TestCase:
     def to_smaller_then(self, expected):
         return self._exec_test('smaller', expected)
 
-    def to_raise(self, expected_type_of_error = Exception):
+    def to_raise(self, expected_type_of_error=Exception):
         return self._exec_test('raise', expected_type_of_error)
 
-    # def to_be_type(self, expected):
-    #     self._check_has_attr()
-    #     status = type(self.given) == expected:
-    #     return self._exec_test(expected, status, 'type')
+    def to_be_type(self, expected):
+        return self._exec_test('type', expected)
 
-    # def to_be_instance(self, expected):
-    #     self._check_has_attr()
-    #     status = instance(self.given, expected)
-    #     return self._exec_test(expected, status, 'instance')
+    def to_be_instance(self, expected):
+        return self._exec_test('instance', expected)
 
-    # def to_has_attr(self, expected):
-    #     self._check_has_attr()
-    #     status = hasattr(self.given, expected)
-    #     return self._exec_test(expected, status, 'has_attr')
+    def to_has_attr(self, expected):
+        return self._exec_test('has_attr', expected)
 
-    # def to_has_method(self, expected):
-    #     self._check_has_attr()
-    #     status = hasattr(self.given, expected) and callable(getattr(self.given, expected))
-    #     return self._exec_test(expected, status, 'has_method')
-
+    def to_has_method(self, expected):
+        return self._exec_test('has_method', expected)
